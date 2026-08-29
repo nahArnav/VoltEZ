80b55dff751dab20744bee6faf01b9037fc7193f
KD4R <kedar.vaije546@gmail.com>
2026-08-29 18:08:18 +0530

    fix: restore frontend font assets and tests

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -7,17 +7,30 @@ import 'package:voltez_frontend/core/providers/charger_discovery_provider.dart';
 import 'package:voltez_frontend/core/providers/route_planner_provider.dart';
 import 'package:voltez_frontend/core/providers/booking_provider.dart';
 import 'package:voltez_frontend/core/providers/session_provider.dart';
+import 'package:voltez_frontend/core/providers/business_provider.dart';
+import 'package:voltez_frontend/core/network/api_service.dart';
+import 'package:voltez_frontend/core/network/server_config.dart';
 
 void main() {
   testWidgets('App renders without crashing', (WidgetTester tester) async {
     await tester.pumpWidget(
       MultiProvider(
         providers: [
+          Provider<ApiService>(
+            create: (_) => ApiService(baseUrl: 'http://localhost:8000'),
+          ),
+          ChangeNotifierProxyProvider<ApiService, ServerConfig>(
+            create: (ctx) => ServerConfig(api: ctx.read<ApiService>(), initialUrl: 'http://localhost:8000'),
+            update: (_, api, _) => ServerConfig(api: api, initialUrl: 'http://localhost:8000'),
+          ),
           ChangeNotifierProvider(
             create: (_) => AuthProvider(),
           ),
           ChangeNotifierProvider(
             create: (_) => ChargerDiscoveryProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => BusinessProvider(api: ctx.read<ApiService>()),
           ),
           ChangeNotifierProvider(
             create: (_) => RoutePlannerProvider(),
